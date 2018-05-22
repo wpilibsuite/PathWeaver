f7053003798d4ca6081b03832189b8a9f7c389d4
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2018-05-21 20:18:03 -0400

    Convert Gradle build script to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    `java`
+    `application`
+    `pmd`
+    `checkstyle`
+}
+
+group = "edu.wpi.first.path_ui"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+checkstyle {
+    configFile = File(rootDir, "checkstyle.xml")
+    toolVersion = "8.10"
+    if (project.hasProperty("ignoreCheckstyle")) {
+        isIgnoreFailures = true
+    }
+}
+
+pmd {
+    isConsoleOutput = true
+    toolVersion = "6.3.0"
+    reportsDir = file("${project.buildDir}/reports/pmd")
+    ruleSetFiles = files(File(rootDir, "pmd-ruleset.xml"))
+    ruleSets = emptyList()
+}
+
+application {
+    mainClassName = "edu.wpi.first.pathui.PathUI"
+}
