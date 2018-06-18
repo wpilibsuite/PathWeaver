c1889c1a9e9167767b33e6bfc41ca43c8044f6e0
gftabor <tabor473@gmail.com>
2018-06-18 11:44:00 -0400

    Track dragging

--- src/main/java/edu/wpi/first/pathui/MainController.java
+++ src/main/java/edu/wpi/first/pathui/MainController.java
@@ -2,10 +2,17 @@ package edu.wpi.first.pathui;
 
 import java.io.File;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
+import javafx.util.Callback;
 
 public class MainController {
   @FXML private TreeView<String> autons;
@@ -45,6 +52,8 @@ public class MainController {
     pathRoot.setExpanded(true);
     setupPathsInDirectory(directory);
     setupClickablePaths();
+    setupDrag(paths);
+    setupDrag(autons);
   }
 
   private void setupClickablePaths() {
@@ -53,7 +62,7 @@ public class MainController {
         .addListener(
             (observable, oldValue, newValue) -> {
               if (newValue.getValue() == pathRoot.getValue()) {
-                pathRoot.setExpanded(!pathRoot.isExpanded());
+                //pathRoot.setExpanded(!pathRoot.isExpanded());
               } else {
                 if (newValue != null) {
                   pathDisplayController.addPath(directory, newValue.getValue());
@@ -63,6 +72,57 @@ public class MainController {
                 }
               }
             });
+  }
+  private void setupDrag(TreeView<String> tree){
+    tree.setCellFactory(new Callback<>() {
+      @Override
+      public TreeCell<String> call(TreeView<String> stringTreeView) {
+        TreeCell<String> cell = new TreeCell<>() {
+          protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item != null) {
+              setText(item);
+            }else{
+              setText(null);
+            }
+          }
+        };
+
+        cell.setOnDragDetected(event -> {
+          TreeItem<String> item = cell.getTreeItem();
+          if (item != null && item.isLeaf()) {
+            Dragboard db = cell.startDragAndDrop(TransferMode.COPY);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(item.getValue());
+            db.setContent(content);
+            event.consume();
+          }
+        });
+        cell.setOnDragOver(event -> {
+          TreeItem<String> item = cell.getTreeItem();
+          if (item != null &&
+              event.getGestureSource() != cell &&
+              event.getDragboard().hasString()) {
+            String target = cell.getTreeItem().getValue();
+            Object source = event.getGestureSource();
+            //TODO check if duplicate
+
+            if (item.isLeaf()) {
+              System.out.println("ontop of " + target);
+            } else {
+              event.acceptTransferModes(TransferMode.COPY);
+              
+              System.out.println("dragging " + source);
+              System.out.println("over " + target);
+              event.consume();
+
+            }
+          }
+        });
+
+        return cell;
+      }
+    });
   }
 
   private void setupPathTreeItem(String fileName) {
