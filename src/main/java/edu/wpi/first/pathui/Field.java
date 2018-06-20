2500aa72769d343520d4e643fe58019bf3739bfc
Arjun Gandhi <arjungandhi06@gmail.com>
2018-06-20 12:07:09 -0400

    Added a Unit file and implemented it in thefield class

--- src/main/java/edu/wpi/first/pathui/Field.java
+++ src/main/java/edu/wpi/first/pathui/Field.java
@@ -1,71 +1,88 @@
 package edu.wpi.first.pathui;
 
 
-
-import si.uom.SI;
+import javafx.scene.image.Image;
 import tec.units.indriya.quantity.Quantities;
 
-import javafx.scene.image.Image;
-
 import javax.measure.Quantity;
-import javax.measure.UnitConverter;
-import javax.measure.quantity.Length;
 import javax.measure.Unit;
+import javax.measure.quantity.Length;
+import java.awt.geom.Point2D;
 
-import static systems.uom.common.USCustomary.FOOT;
-import static systems.uom.common.USCustomary.METER;
+import static edu.wpi.first.pathui.PathUnits.FOOT;
 
 
 public class Field {
-    private Image image= new Image("edu/wpi/first/pathui/2018-field.jpg");
-    Quantity<Length> widthReal = Quantities.getQuantity(54,FOOT);
-    Quantity<Length> heightReal = Quantities.getQuantity(27,FOOT);
-    private int x=0;
-    private int y=0;
+    private Image image = new Image("edu/wpi/first/pathui/2018-field.jpg");
+    private Quantity<Length> rWidth = Quantities.getQuantity(54,FOOT);
+    private Quantity<Length> rLength = Quantities.getQuantity(27,FOOT);
+    private double scale = 100;
+    private Point2D coord = new Point2D.Double(0, 0);
 
 
-    public Field(){
 
+    Unit<Length> unit = FOOT;
+
+    public Field() {
 
     }
+
+    public Field(Image image,Unit<Length> u, double realWidth, double realLength, double xPixel, double yPixel, double pixelWidth, double pixelLength) {
+        setImage(image);
+        setRealWidth(Quantities.getQuantity(54,FOOT));
+        setRealLength(Quantities.getQuantity(27,FOOT));
+        setCoord(new Point2D.Double(xPixel,yPixel));
+        setScale(((pixelWidth/realWidth)+(pixelLength/realLength))/2);
+        setUnit(u);
+        Unit<Length> Pixel = PathUnits.addUnit(unit.multiply(scale),"Pixel","pix");
+    }
+
 
     public Image getImage() {
         return image;
     }
 
-    public void setImage(Image image) {
+    private void setImage(Image image) {
         this.image = image;
     }
 
-    public Number getRealWidth() {
-        return widthReal.getValue();
+    public Quantity<Length> getRealWidth() {
+        return rWidth;
     }
 
-    public void setRealWidth(double width,String unit) {
-        this.widthReal = Quantities.getQuantity(width, unit);
+    private void setRealWidth(Quantity<Length> width) {
+        this.rWidth = width;
     }
 
-    public double getRealHeight() {
-        return height;
+    public Quantity<Length> getRealLength() {
+        return rLength;
     }
 
-    public void setHeight(double height) {
-        this.height = height;
+    private void setRealLength(Quantity<Length> length) {
+        this.rLength = length;
     }
 
-    public int getX() {
-        return x;
+    public double getScale() {
+        return scale;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    private void setScale(double scale) {
+        this.scale = scale;
     }
 
-    public int getY() {
-        return y;
+    public Point2D getCoord() {
+        return coord;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    private void setCoord(Point2D coord) {
+        this.coord = coord;
+    }
+
+    public Unit<Length> getUnit() {
+        return unit;
+    }
+
+    private void setUnit(Unit<Length> unit) {
+        this.unit = unit;
     }
 }
---
+++ src/main/java/edu/wpi/first/pathui/PathUnits.java
@@ -0,0 +1,59 @@
+package edu.wpi.first.pathui;
+
+import systems.uom.common.Imperial;
+import systems.uom.common.USCustomary;
+import tec.units.ri.AbstractSystemOfUnits;
+import tec.units.ri.AbstractUnit;
+import tec.units.ri.format.SimpleUnitFormat;
+
+import javax.measure.Unit;
+import javax.measure.quantity.Length;
+import javax.measure.quantity.Time;
+import javax.measure.spi.SystemOfUnits;
+
+import static tec.units.ri.unit.Units.METRE;
+
+public class PathUnits extends AbstractSystemOfUnits {
+    private static final String SYSTEM_NAME = "Path UI Units";
+
+    private PathUnits() {
+    }
+
+    @Override
+    public String getName() {
+        return SYSTEM_NAME;
+    }
+
+    public static SystemOfUnits getInstance() {
+        return INSTANCE;
+    }
+
+    private static final PathUnits INSTANCE = new PathUnits();
+
+    public static final Unit<Length> METER = addUnit(USCustomary.METER, "Meter", "m");
+    public static final Unit<Length> INCH = addUnit(USCustomary.INCH, "Inch", "in");
+    public static final Unit<Length> FOOT = addUnit(USCustomary.FOOT, "Foot", "ft");
+    public static final Unit<Length> YARD = addUnit(USCustomary.YARD, "Yard", "yd");
+    public static final Unit<Length> MILE = addUnit(USCustomary.MILE, "Mile", "mi");
+    public static final Unit<Length> CENTIMETER = addUnit(METER.divide(100), "Centimeter", "cm");
+    public static final Unit<Length> MILLIMETER = addUnit(METER.divide(100), "Millimeter", "mm");
+
+    public static final Unit<Time> MINUTE = addUnit(USCustomary.MINUTE, "Minute", "min");
+    public static final Unit<Time> HOUR = addUnit(USCustomary.HOUR, "Hour", "hr");
+
+    public static <U extends Unit<?>> U addUnit(U unit, String name, String label) {
+        return addUnit(unit, name, label, true);
+    }
+
+    private static <U extends Unit<?>> U addUnit(U unit, String name, String text, boolean isLabel) {
+        if (isLabel) {
+            SimpleUnitFormat.getInstance().label(unit, text);
+        }
+        if (name != null && unit instanceof AbstractUnit) {
+            return Helper.addUnit(INSTANCE.units, unit, name);
+        } else {
+            INSTANCE.units.add(unit);
+        }
+        return unit;
+    }
+}
