d9d22d2f0e2d23f6453853487f6b62ea6e6940cc
Arjun Gandhi <arjungandhi06@gmail.com>
2018-07-05 15:54:16 -0400

    Set up basics for reading and writing  to json

---
+++ src/main/java/edu/wpi/first/pathui/ProjectUtils.java
@@ -0,0 +1,37 @@
+package edu.wpi.first.pathui;
+
+
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ProjectUtils {
+
+  private static JSONObject field = null;
+  private static JSONObject robot = null;
+
+  public static void exportField(JSONObject object, String savePath) throws IOException {
+    FileWriter file = new FileWriter(savePath + "/Field/field.json");
+    file.write(object.toString(2));
+  }
+
+  public static void exportRobot(JSONObject object, String savePath) throws IOException {
+    FileWriter file = new FileWriter(savePath + "/Robot/robot.json");
+    file.write(object.toString(2));
+  }
+
+  public static void importField(String savePath) throws FileNotFoundException {
+    FileReader reader = new FileReader(savePath + "/Field/field.json");
+    String str = reader.toString();
+    field = new JSONObject(str);
+  }
+
+  public static void importRobot(String savePath) throws FileNotFoundException {
+    FileReader reader = new FileReader(savePath + "/Robot/robot.json");
+    String str = reader.toString();
+    robot = new JSONObject(str);
+  }
+}
