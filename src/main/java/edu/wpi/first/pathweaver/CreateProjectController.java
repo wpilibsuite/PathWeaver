92dc6271ae6b33531c1d1578a86cddba080d8178
Kevin O'Brien <kjrobrien@gmail.com>
2018-08-01 17:03:39 -0400

    Add controller for creating project

--- src/main/java/edu/wpi/first/pathweaver/CreateProjectController.java
+++ src/main/java/edu/wpi/first/pathweaver/CreateProjectController.java
@@ -1,11 +1,49 @@
 package edu.wpi.first.pathweaver;
 
+import java.io.File;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
 
 public class CreateProjectController {
 
   @FXML
-  private void initialize() {
+  VBox vBox;
+  @FXML
+  TextField directory;
+  @FXML
+  Button browse;
+  @FXML
+  TextField timeStep;
+  @FXML
+  TextField maxVelocity;
+  @FXML
+  TextField maxAcceleration;
+  @FXML
+  TextField maxJerk;
+  @FXML
+  TextField wheelBase;
+  @FXML
+  Button create;
 
+  @FXML
+  private void initialize() {
+    // TODO: validate numbers only
+    browse.setOnAction(event -> {
+      browseDirectory();
+    });
+  }
+
+  private void browseDirectory() {
+    DirectoryChooser chooser = new DirectoryChooser();
+    File selectedDirectory = chooser.showDialog(vBox.getScene().getWindow());
+    if (selectedDirectory != null) {
+      directory.setText(selectedDirectory.getPath());
+    }
   }
 }
