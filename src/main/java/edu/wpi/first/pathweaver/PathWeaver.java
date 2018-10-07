4f467f0a0bbe2665d1258511bfd20af913110a1e
Kevin O'Brien <kobrien2@wpi.edu>
2018-10-06 20:30:24 -0400

    Dynamically set PathWeaver version

--- src/main/java/edu/wpi/first/pathweaver/PathWeaver.java
+++ src/main/java/edu/wpi/first/pathweaver/PathWeaver.java
@@ -11,11 +11,24 @@ import javafx.stage.Stage;
 public class PathWeaver extends Application {
   @Override
   public void start(Stage primaryStage) throws IOException {
+    System.out.println(PathWeaver.class.getPackage().getImplementationVersion());
     Pane root = FXMLLoader.load(getClass().getResource("welcomeScreen.fxml"));
     Scene scene = new Scene(root);
     primaryStage.resizableProperty().setValue(false);
     primaryStage.setScene(scene);
     primaryStage.show();
     Loggers.setupLoggers();
+  }
+
+  /**
+   * The version of this build of PathWeaver.
+   * @return String representing the version of PathWeaver.
+   */
+  public static String getVersion() {
+    String version = PathWeaver.class.getPackage().getImplementationVersion();
+    if (version == null) {
+      return "Development";
+    }
+    return version;
   }
 }
