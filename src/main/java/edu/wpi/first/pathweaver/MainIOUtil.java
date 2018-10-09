9ffea1f29730ba5cb6cfb2735ceb771f03ca3788
Kevin O'Brien <kobrien2@wpi.edu>
2018-10-08 20:47:58 -0400

    Checkstyle

--- src/main/java/edu/wpi/first/pathweaver/MainIOUtil.java
+++ src/main/java/edu/wpi/first/pathweaver/MainIOUtil.java
@@ -45,10 +45,10 @@ public final class MainIOUtil {
    */
   public static String getValidFileName(String directory, String filename, String extension) {
     // remove the _number following the file name
-    filename = filename.replaceFirst("_[0-9]+", "");
-    File file = new File(directory, filename + extension);
+    String nameNoVersion = filename.replaceFirst("_[0-9]+", "");
+    File file = new File(directory, nameNoVersion + extension);
     for (int num = 0; file.exists(); num++) {
-      file = new File(directory, filename + "_" + num + extension);
+      file = new File(directory, nameNoVersion + "_" + num + extension);
     }
     return file.getName();
   }
