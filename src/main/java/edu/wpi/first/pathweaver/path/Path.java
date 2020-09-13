ea2d5b349244aea083f7813cffb432e6009fbb15
Carlin Williamson <carlin.williamson1@gmail.com>
2020-09-13 21:00:54 +1000

    removed extraneous print

--- src/main/java/edu/wpi/first/pathweaver/path/Path.java
+++ src/main/java/edu/wpi/first/pathweaver/path/Path.java
@@ -133,7 +133,6 @@ public abstract class Path {
     }
 
     public Waypoint addWaypoint(Point2D coordinates, Waypoint start, Waypoint end) {
-        System.out.println(waypoints.size());
         for (int i = 1; i < waypoints.size(); i++) {
             if (waypoints.get(i - 1).equals(start) && waypoints.get(i).equals(end)) {
                 Waypoint toAdd = new Waypoint(coordinates, new Point2D(0, 0), false, false);
